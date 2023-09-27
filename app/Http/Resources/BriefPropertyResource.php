1cfafd3308982a4d7a4df1fb46c36c8f3f8722a2
Huzaifa353 <huzefashamoon@gmail.com>
2023-09-27 06:35:03 -0700

    Floor Plans and Advanced Search data added to Property APIs

--- app/Http/Resources/BriefPropertyResource.php
+++ app/Http/Resources/BriefPropertyResource.php
@@ -14,22 +14,28 @@ class BriefPropertyResource extends JsonResource
      */
     public function toArray($request)
     {
+        $features = $this->propertyFeatures->pluck('prop_feature');
         return [
             'id' => $this->id,
             'name' => $this->prop_name,
+            'code' => $this->prop_code,
+            'city' => $this->city_id,
             'image' => $this->prop_image,
             'type' => $this->prop_type,
             'address' => $this->prop_address,
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
             'status' => $this->prop_status,
-            'est_occupancy_year' => $this->est_occupancy_year,
+            'yearBuilt' => $this->est_occupancy_year,
             'vip_featured_promotion' => $this->vip_featured_promotion,
             'isHot' => $this->is_hot,
             'beds' => $this->no_of_beds,
             'baths' => $this->no_of_baths,
             'price_from' => $this->prop_price_from,
             'price_to' => $this->prop_price_to,
+            'areaMin' => $this->suite_size_from,
+            'areaMax' => $this->suite_size_to,
+            'features' => $features,
             'agent' => BriefAgentResource::make($this->property_agent),
         ];
     }
